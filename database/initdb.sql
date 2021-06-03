72b666d09c1edfb52f1451b92ed523e60f0f261e
Rafael Aviles <rafa.vfierro@outlook.com>
2021-06-02 21:07:26 -0700

    refactor: :card_file_box: Refactor data tables
    Tables now correctly reference each other, and behave how they should.

--- database/initdb.sql
+++ database/initdb.sql
@@ -1,11 +1,21 @@
-CREATE TABLE IF NOT EXISTS highscores(
-    highscore_uid UUID PRIMARY KEY,
-    score NUMERIC(6, 2) NOT NULL CHECK (score > 0)
-);
-
 CREATE TABLE IF NOT EXISTS players(
     player_uid UUID PRIMARY KEY,
-    player_name CHAR(10) UNIQUE NOT NULL,
-    highscore_uid UUID REFERENCES highscores(highscore_uid) ON DELETE CASCADE NOT NULL,
-    CONSTRAINT FK_Highscores UNIQUE (highscore_uid)
+    player_name VARCHAR(15) NOT NULL,
+    country VARCHAR,
+    salt CHAR(16) NOT NULL,
+    pass_hash VARCHAR NOT NULL,
+    UNIQUE(player_name)
+);
+
+CREATE TABLE IF NOT EXISTS sentences(
+    sentence_uid UUID PRIMARY KEY,
+    sentence VARCHAR NOT NULL,
+    UNIQUE(sentence)
+);
+
+CREATE TABLE IF NOT EXISTS scores(
+player_uid UUID REFERENCES players(player_uid) ON DELETE CASCADE,
+sentence_uid UUID REFERENCES sentences(sentence_uid) ON DELETE CASCADE,
+score NUMERIC(6,2) NOT NULL CHECK(score > 0),
+PRIMARY KEY(player_uid, sentence_uid)
 );

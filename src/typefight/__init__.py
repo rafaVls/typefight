40d6108fee5302ccb0d04e0dd98fd14677158c3d
Rafael Aviles <rafa.vfierro@outlook.com>
2021-06-02 21:14:19 -0700

    refactor: :fire: Remove unused lines from __init__.py

--- src/typefight/__init__.py
+++ src/typefight/__init__.py
@@ -1,13 +1,7 @@
-import os
 from flask import Flask
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
     
     @app.route("/hello")
     def hello():

f91861de364c794f1d54962042956ccfa1c34ae3
Rafael Aviles <rafa.vfierro@outlook.com>
2021-06-03 11:14:16 -0700

    feat: :sparkles: Add db module for opening and closing conections to the database
    The connect() method takes environment variables from the os, which are specified in the docker-compose.yaml file, which in turn takes them from a .env file

---
+++ src/typefight/db.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import current_app, g
+import psycopg2
+
+def get_db():
+    if "db" not in g:
+        g.db = psycopg2.connect(
+            user=os.environ["DB_USER"],
+            database=os.environ["DB_NAME"],
+            password=os.environ["DB_PASSWORD"],
+            host="database"
+        )
+    return g.db
+
+def close_db():
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()

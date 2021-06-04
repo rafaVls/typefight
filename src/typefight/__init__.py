d77cfa64696e3810f63b36c80232877ff5e52842
Rafael Aviles <rafa.vfierro@outlook.com>
2021-06-04 12:33:06 -0700

    feat: :sparkles: Add auth blueprint and register it in the factory app.

--- src/typefight/__init__.py
+++ src/typefight/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from . import auth
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
@@ -6,5 +7,7 @@ def create_app():
     @app.route("/hello")
     def hello():
         return "Hello, world!"
+
+    app.register_blueprint(auth.bp)
     
     return app
---
+++ src/typefight/auth.py
@@ -0,0 +1,8 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+from typefight.db import get_db, close_db
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")

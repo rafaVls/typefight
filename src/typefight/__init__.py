56c1dc740be40b9913cf2ba6e194b54ba3c0a806
Rafael Aviles <rafa.vfierro@outlook.com>
2021-06-04 18:31:12 -0700

    fix: :card_file_box: Add close_db function to teardown_appcontext
    This will call close_db after for each request, on cleanup.

--- src/typefight/__init__.py
+++ src/typefight/__init__.py
@@ -1,13 +1,14 @@
+import os
+
 from flask import Flask
-from . import auth
+from . import auth, db
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    
-    @app.route("/hello")
-    def hello():
-        return "Hello, world!"
+    app.config["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"]
 
     app.register_blueprint(auth.bp)
     
+    # this tells flask to call close_db on cleanup
+    app.teardown_appcontext(db.close_db)
     return app

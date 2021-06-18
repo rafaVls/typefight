3b9be23136fc96e85393d85c3050fadf79aa306d
Rafael Avilés <rafa.vfierro@outlook.com>
2021-06-18 11:35:32 -0700

    feat(Static): :sparkles: Add game functionality to typed input and start button

--- src/typefight/static/js/index.js
+++ src/typefight/static/js/index.js
@@ -1,6 +1,11 @@
 const timer = document.getElementById("timer");
 const startBtn = document.getElementById("start-btn");
-const typedValue = document.getElementById("typed-value");
+const quoteElement = document.getElementById("quote");
+const typedValueElement = document.getElementById("typed-value");
+
+const quote = 'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.';
+let words = [];
+let wordIndex = 0;
 
 let minutes = 0;
 let seconds = 0;
@@ -8,10 +13,20 @@ let start = 0;
 let timerInterval;
 
 function startGame() {
-    typedValue.focus();
     start = new Date().getTime();
+    timer.innerText = "00:00";
     startBtn.innerText = "Restart";
     startBtn.setAttribute("onclick", "resetGame()");
+
+    typedValueElement.addEventListener("input", gameManager);
+    words = quote.split(' ');
+    wordIndex = 0;
+
+    const spanWords = words.map(word => `<span>${word} </span>`)
+    quoteElement.innerHTML = spanWords.join('');
+    quoteElement.childNodes[0].className = "highlight";
+    typedValueElement.value = "";
+    typedValueElement.focus();
 
     timerInterval = setInterval(() => {
         seconds++;
@@ -32,9 +47,30 @@ function resetGame() {
 
     startBtn.setAttribute("onclick", "startGame()");
     startBtn.innerText = "Start";
-    timer.innerText = "00:00";
 
     seconds = 0;
     minutes = 0;
     start = 0;
+}
+
+function gameManager(e) {
+    const currentWord = words[wordIndex];
+    const typedValue = typedValueElement.value;
+
+    if (typedValue === currentWord && wordIndex === words.length - 1) {
+        resetGame();
+        startBtn.focus();
+        typedValueElement.remove();
+        typedValueElement.removeEventListener("input", gameManager);
+    } else if (typedValue.endsWith(" ") && typedValue.trim() === currentWord) {
+        typedValueElement.value = "";
+        wordIndex++;
+
+        for (const wordElement of quoteElement.childNodes) {
+            wordElement.className = "";
+        }
+        quoteElement.childNodes[wordIndex].className = "highlight";
+    } else if (currentWord.startsWith(typedValue)) {
+        typedValueElement.className = "";
+    }
 }

d8b79f56dc1c7673b5a8eb2c41ec3f5ba28fb115
Rafael Avilés <rafa.vfierro@outlook.com>
2021-06-21 07:35:06 -0700

    feat: :bulb: Add description, parameter type and return type to decimal_type_handler
    This function is used when calling json.dumps on the database's "scores" table. Since the table returns the score as a decimal, whe need to convert it to a json-serializable string (we could convert it to a float, but we don't want to lose accuracy).

--- src/typefight/utils.py
+++ src/typefight/utils.py
@@ -1,6 +1,12 @@
 from decimal import Decimal
 
-def decimal_type_handler(decimal_value):
+def decimal_type_handler(decimal_value: Decimal) -> str:
+    """
+    Used as type handler when using json.dumps.
+    It'll take a non json-serializable Decimal type and convert
+    it to a json-serializable string type.
+    """
+    
     if isinstance(decimal_value, Decimal):
         return str(decimal_value)
     raise TypeError

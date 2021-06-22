0ee1cfc9413c4f88b0469435466ae59a16545a19
Rafael Avilés <rafa.vfierro@outlook.com>
2021-06-21 21:33:32 -0700

    refactor: :recycle: Refactor decimal_type_handler into two separate utilities.
    These two functions will be used with flask's jsonify and psycopg2's cursor to transform data tables into json objects.

--- src/typefight/utils.py
+++ src/typefight/utils.py
@@ -1,12 +1,30 @@
 from decimal import Decimal
 
-def decimal_type_handler(decimal_value: Decimal) -> str:
+def make_serializable(data:list) -> list:
     """
-    Used as type handler when using json.dumps.
-    It'll take a non json-serializable Decimal type and convert
-    it to a json-serializable string type.
+    Returns a json-serializable list that can be 
+    used as an http response.
     """
+
+    serialized_list = []
+
+    for raw_row in data:
+        processed_row = {}
+
+        for column_value in raw_row:
+            processed_row[column_value] = decimal_type_handler(raw_row[column_value])
+
+        serialized_list.append(processed_row)
     
-    if isinstance(decimal_value, Decimal):
-        return str(decimal_value)
-    raise TypeError
+    return serialized_list
+
+def decimal_type_handler(object_to_check: (Decimal | any)) -> (float | any):
+    """
+    Checks if object_to_check is of type decimal;
+    if it is, casts it into a float.
+    """
+
+    if isinstance(object_to_check, Decimal):
+        return float(object_to_check)
+
+    return object_to_check

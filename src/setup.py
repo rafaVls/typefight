beeb2940d93f747cfabfb446ba9cea7afdaddffa
Rafael Avilés <rafa.vfierro@outlook.com>
2021-07-27 17:39:24 -0700

    chore: :wrench: Add configuration files needed to make typefight an installable pip package
    This is done so typefight can be imported from outside its folder. Mainly so it can be imported and used in testing.

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from os import name
+from setuptools import find_packages, setup
+
+setup(
+    name="typefight",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "flask",
+    ],
+)

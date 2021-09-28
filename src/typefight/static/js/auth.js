3ff5b5a36b466798059dc0b54f0511704fa14b95
Rafael Avilés <rafa.vfierro@outlook.com>
2021-09-28 16:43:55 -0700

    feat(incomplete): add code to switch the forms if user goes to /auth/login

--- src/typefight/static/js/auth.js
+++ src/typefight/static/js/auth.js
@@ -3,6 +3,12 @@ import { switchForm } from "./utils.js";
 const buttonsContainer = document.getElementById("buttons-container");
 const [...buttons] = buttonsContainer.children;
 
-buttons.forEach((button) => button.addEventListener("click", function () {
+buttons.forEach((button) =>
+  button.addEventListener("click", function () {
     switchForm(this, buttonsContainer);
-}))
+  })
+);
+
+if (document.getElementById("login-btn").hasAttribute("data-login")) {
+  buttons[0].click();
+}
